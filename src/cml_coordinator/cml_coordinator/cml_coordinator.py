046c815e99fd51cd9d67e1d4d618f6449b9a2f6c
Ken4027 <is0800ip@ed.ritsumei.ac.jp>
2026-04-21 16:55:20 +0900

    Implement TTS for robot speech output
    Added text-to-speech functionality to the robot by creating a new publisher for robot speech messages.

--- src/cml_coordinator/cml_coordinator/cml_coordinator.py
+++ src/cml_coordinator/cml_coordinator/cml_coordinator.py
@@ -11,6 +11,9 @@ class CMLCoordinator(Node):
         #Publishers
         self.state_pub = self.create_publisher(String, '/cml_state', 10)
 
+        # TTS(Publish messages robot needs to say)  
+        self.pub_tts = self.create_publisher(String, '/robot_speech', 10)
+
         #Subscribers
         self.command_sub = self.create_subscription(String, '/voice_imperatives', self.command_callback, 10)
 
@@ -21,10 +24,12 @@ class CMLCoordinator(Node):
         if msg.data == "following":
             self.state = "FOLLOW"
             self.get_logger().info("Switching to FOLLOW")
+            self.say("I'm ready. Please start walking, I will follow you.")
 
         elif msg.data == "returning":
             self.state = "RETURN"
             self.get_logger().info("Switching to RETURN")
+            self.say("I will return to the start point now.")
 
         self.publish_state()
 
@@ -32,6 +37,13 @@ class CMLCoordinator(Node):
         msg = String()
         msg.data = self.state
         self.state_pub.publish(msg)
+
+    def say(self, text):
+        """Make Robot Speak"""
+        self.get_logger().info(f"ROBOT SAYS: {text}")
+        msg = String()
+        msg.data = text
+        self.pub_tts.publish(msg)
 
 
 def main(args=None):
